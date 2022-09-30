147b54b93e68f3747f8104ca325fbbc76115adb9
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2022-09-30 08:09:10 +0100

    Extract virtual page view function to actions

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,7 @@ import { setCustomer } from '../store/actions/authenticateActions';
 import 'swiper/components/effect-fade/effect-fade.scss';
 import { useRouter } from 'next/router'
 import { route } from 'next/dist/server/router';
-import { virtualPageView } from '../store/actions/analyticsActions';
-
-const gtmVirtualPageView = (rest) => {
-  window.dataLayer?.push({
-    event: 'virtualPageView',
-    _clear: true,
-    ...rest,
-  });
-};
+import { virtualPageView } from '../store/actions/analyticsActions';s
 
 const MyApp = ({Component, pageProps}) => {
 
